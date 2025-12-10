cdc6a72982bf79ee5248d723f875d2a59fdf4df3
Anastasiia Pohonets <29karasik@gmail.com>
2025-12-10 12:36:09 +0200

    add build.sh and freeze requirements.txt

--- Kitchen_Service/settings.py
+++ Kitchen_Service/settings.py
@@ -17,9 +17,13 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # DEBUG = False
 
