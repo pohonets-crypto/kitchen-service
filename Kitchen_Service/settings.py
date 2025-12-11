fad3f7e5ac6a27e6eb85d720a3bc5f482ce2af57
Anastasiia Pohonets <29karasik@gmail.com>
2025-12-11 18:29:14 +0200

    changed debug to false

--- Kitchen_Service/settings.py
+++ Kitchen_Service/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
